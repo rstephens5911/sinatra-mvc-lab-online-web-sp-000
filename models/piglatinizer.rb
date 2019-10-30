069f0c94c5346801aa93ce4343a26fd020d559d1
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-30 23:22:38 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer
+
+  def to_pig_latin(phrase)
+    words = phrase.split(" ")
+    words.collect { |w| piglatinize(w) }.join(" ")
+  end
+
+  def piglatinize(word)
+    parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
+    start = parts_of_word[1]
+    rest = parts_of_word[2] + (parts_of_word[3] || "")
+    if start.length > 0
+      "#{rest}#{start}ay"
+    else
+      "#{rest}way"
+    end
+  end
+end
